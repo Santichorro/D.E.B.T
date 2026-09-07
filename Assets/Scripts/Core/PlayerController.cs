eb243f9c7a9a824c6f2280b5ef67f6956e94dffe
Santichorro <sntgtello01@gmail.com>
2026-09-07 11:32:02 -0500

    Multijugador Local y NIS listo
    Se agregó el NIS y se agregó el multijugador local

--- Assets/Scripts/Core/PlayerController.cs
+++ Assets/Scripts/Core/PlayerController.cs
@@ -1,16 +1,70 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movimiento (fuerzas)")]
+    public float thrustForce = 15f;
+    public float maxSpeed = 8f;
+
+    [Header("Rotación hacia el movimiento")]
+    public float rotationSpeed = 360f;
+    public float minSpeedToRotate = 0.15f;
+    public float anguloOffset = -90f;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    private NIS inputActions;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.constraints = RigidbodyConstraints.FreezePositionZ
+                        | RigidbodyConstraints.FreezeRotationX
+                        | RigidbodyConstraints.FreezeRotationY;
+
+        inputActions = new NIS();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        var playerInput = GetComponent<PlayerInput>();
+        inputActions.devices = playerInput.devices;
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Move.canceled += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Move.canceled -= OnMove;
+        inputActions.Player.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext ctx)
+    {
+        moveInput = ctx.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 inputDir = new Vector3(moveInput.x, moveInput.y, 0f);
+        rb.AddForce(inputDir * thrustForce, ForceMode.Force);
+        rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, maxSpeed);
+
+        if (rb.linearVelocity.magnitude > minSpeedToRotate)
+        {
+            float angle = Mathf.Atan2(rb.linearVelocity.y, rb.linearVelocity.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle + anguloOffset);
+            rb.MoveRotation(Quaternion.RotateTowards(rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime));
+        }
     }
 }
